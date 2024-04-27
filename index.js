c81e99ae179c44a3cb1b87a0f81791db0f454cf8
vaishnavihole <vaishnavihole1@gmail.com>
2024-04-27 11:15:27 +0530

    multer setup and uplaod image api added

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 import express from 'express';
 import dotennv from 'dotenv';
 import mongoose from 'mongoose';
+import uploadCloudinary from './utils/cloudinary.js';
+import fs from 'fs';
+import multer from 'multer';
 
 dotennv.config();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const uploadPath = "./public/temp";
+        fs.mkdirSync(uploadPath, { recursive: true });
+        cb(null, uploadPath);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+
+const upload = multer({ storage });
 const app = express();
 app.use(express.json());
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+    try {
+
+        const localFilePath = req.file.path;
+        const response = await uploadCloudinary(localFilePath);
+        console.log(localFilePath);
+        console.log(response);
+
+        if (response) {
+            res.status(200).json({ url: response.url });
+        } else {
+            res.status(500).json({ error: "Failed to upload file to Cloudinary" });
+        }
+    } catch (error) {
+        console.error("Error uploading file:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 
 const PORT = process.env.PORT || 5000;
 
---
+++ utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null
+    //uplaod the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    })
+    //  file has been uploaded successfully
+    console.log("File uploaded successfully",
+      response.url);
+    return response;
+
+  } catch (error) {
+    // remove locally saved  temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+}
+
+export default uploadCloudinary;
